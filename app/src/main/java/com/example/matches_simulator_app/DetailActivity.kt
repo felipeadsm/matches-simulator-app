92db88a86e0e6e01920984705cb27be5e61df71a
felipeadsm <97059009+felipeadsm@users.noreply.github.com>
2022-07-02 15:34:39 -0300

    update: construção das telas

---
+++ app/src/main/java/com/example/matches_simulator_app/DetailActivity.kt
@@ -0,0 +1,20 @@
+package com.example.matches_simulator_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.matches_simulator_app.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDetailBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+}
